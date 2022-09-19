c904f46b76f76c01c3e5bf81de9af42881e524a0
= <=>
2022-09-19 12:30:35 +0200

    prova.ps1

---
+++ prova.ps1
@@ -0,0 +1,19 @@
+﻿$urlOriginal = "https://noixion.tv/videos/player/iMjxwrPAwSMgU_erS-rbQg"
+
+$baseUrl = "https://noixion.tv/api/videos/hls/"
+
+$arrayUrl = $urlOriginal.split('/')
+$logitud = $arrayUrl.Length - 1
+$apiKey = $arrayUrl[$logitud]
+
+$indexUrl = "?file=index0"
+
+$finUrl = ".ts&res=1080P"
+
+$indice = 0
+
+$indiceStr = $indice.ToString()
+
+$url = "$baseUrl$apiKey$indiceStr$finUrl"
+
+Write-Host "$url"
